97d9b1fc84af69abe8db15163d47fe8d35b18d25
nnnayy <kny5439@cau.ac.kr>
2022-09-22 21:42:54 +0900

    Initial Commit

---
+++ assignment/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  assignment
+//
+//  Created by 김나예 on 2022/09/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
